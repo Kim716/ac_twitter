3656b67f27d1612e4975fe403f7bebd3dfcef640
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-03-20 18:11:46 +0800

    feat: add Logo component

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,12 @@
+import Logo from "../components/Logo";
+
 function LoginPage() {
-  return <h1>LoginPage</h1>;
+  return (
+    <>
+      <h1>LoginPage</h1>
+      <Logo width="50px" />
+    </>
+  );
 }
 
 export default LoginPage;
