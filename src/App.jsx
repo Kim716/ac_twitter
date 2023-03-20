7e3191002b486a52d3136c5fbe0dda1f91291cae
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-03-20 10:58:57 +0800

    feat: initialize project file & install packages
    將 CRA 產生的不必要檔案進行整理，
    先下載後續會用到的相關套件，方便共同開發版本的統一。

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "bootstrap/dist/css/bootstrap-grid.min.css";
+import styled from "styled-components";
+
+const StyledH1 = styled.h1`
+  color: red;
+`;
+
+function App() {
+  return (
+    <div>
+      <StyledH1>Hello</StyledH1>
+    </div>
+  );
+}
+
+export default App;
