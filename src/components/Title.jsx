39cb6b586d6105b028b3bebc0ce5319f331a1714
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-03-21 12:08:54 +0800

    style: edit Title component margin

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 
 const StyledH1 = styled.h1`
+  margin: 30px 0 40px;
   font-weight: 700;
   font-size: 28px;
   color: #1c1c1c;
+  text-align: center;
 `;
 
 function Title({ children }) {
