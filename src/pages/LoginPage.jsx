18587312637f9fbd1224bcba90465468ce0ac2ff
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-03-21 11:20:20 +0800

    feat: add Input component
    已設計 default/hover/focus 樣式

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,11 +1,24 @@
 import Logo from "components/Logo";
 import Title from "components/Title";
+import Input from "components/Input";
 
 function LoginPage() {
   return (
     <>
       <Logo width="50px" />
       <Title>登入 Alphitter</Title>
+      <Input
+        id="login_account"
+        label="帳號"
+        type="text"
+        placeholder="請輸入帳號"
+      />
+      <Input
+        id="login_password"
+        label="密碼"
+        type="password"
+        placeholder="請輸入密碼"
+      />
     </>
   );
 }
