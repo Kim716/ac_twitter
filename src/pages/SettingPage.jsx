27c581f8c8d57e7037c3c0e6ba777611da27bbec
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-03-21 18:21:51 +0800

    fix: redo c0~c6

--- src/pages/SettingPage.jsx
+++ src/pages/SettingPage.jsx
@@ -1,5 +1,17 @@
+import NavContainer from "components/containers/NavContainer";
+import MainContainer from "components/containers/MainContainer";
+
 function SettingPage() {
-  return <h1>SettingPage</h1>;
+  return (
+    <div className="d-flex">
+      <NavContainer>
+        <h1>SettingPage</h1>
+      </NavContainer>
+      <MainContainer>
+        <h1>Main</h1>
+      </MainContainer>
+    </div>
+  );
 }
 
 export default SettingPage;
