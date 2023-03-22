38bb8ec49de8cd15abb11682a474c6ca47866860
violet120 <ai1397de@gmail.com>
2023-03-22 15:00:16 +0800

    feat: change LinkItem logic 登出按鈕可選擇換色還是不換色，決定後請再協助更新或通知我修改

--- src/components/LinkItem.jsx
+++ src/components/LinkItem.jsx
@@ -5,9 +5,12 @@ import { ReactComponent as SetUnfocus } from "assets/icons/set_unfocus.svg"
 import { ReactComponent as SetFocus } from "assets/icons/set_focus.svg"
 import { ReactComponent as UserUnfocus } from "assets/icons/user_unfocus.svg"
 import { ReactComponent as UserFocus } from "assets/icons/user_focus.svg"
+import { ReactComponent as Logout } from "assets/icons/logout_unfocus.svg"
 import { useState } from "react";
 
-const StyledIcon = styled.div`
+const StyledIcon = styled.div.attrs((props) => ({
+  className: props.className || ''
+}))`
   display: flex;
   align-items: center;
   width: 178px;
@@ -25,20 +28,45 @@ const StyledIcon = styled.div`
     padding-top: 5px;
     color: ${(props) => props.isClick ? 'var(--brand-color)' : 'var(--grey9)'};
   }
+
+  // 設定登出樣式
+  &.active {
+    // 以下是不會換顏色的版本
+    // & span {
+    //   color: var(--grey9);
+    // }
+    // 以下是會換色的版本
+    color: ${(props) => props.isClick ? 'var(--brand-color)' : 'var(--grey9)'};
+    & svg {
+      stroke: ${(props) => props.isClick ? 'var(--brand-color)' : ''};
+    }
+  }
 `
 
 
-function Link() {
+// 接上換頁功能後，要思考按鈕一個亮，其他的不亮
+
+function Link({ title }) {
   const [isClick, setIsClick] = useState(false)
 
   const handleIsClick = () => {
     setIsClick(!isClick)
   }
 
+  if(title === '登出') {
+    return (
+      <StyledIcon isClick={isClick} onClick={handleIsClick} className="active">
+        <Logout /><span>{title}</span>
+      </StyledIcon>
+    )
+  }
+
   return (
     <StyledIcon isClick={isClick} onClick={handleIsClick}>
-        {isClick ? <SetFocus /> : <SetUnfocus />}
-        <span>設定</span>
+      { title === '首頁' && (isClick ? <HomeFocus /> : <HomeUnfocus />) }
+      { title === '個人資料' && (isClick ? <UserFocus /> : <UserUnfocus />) }
+      { title === '設定' && (isClick ? <SetFocus /> : <SetUnfocus />) }
+      <span>{ title }</span>
     </StyledIcon>
   )
 }
