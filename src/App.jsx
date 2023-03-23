bdc5e8f73068153eb5b7785f0cc5aad0bb5665b2
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-03-23 18:22:42 +0800

    fix: nest route

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./styles/base.css";
 import "bootstrap/dist/css/bootstrap-grid.min.css";
 
 // Package
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 
 // Pages
 import {
@@ -22,40 +22,48 @@ import {
   UserMainPage,
   UserReplysPage,
 } from "./pages";
+import { useEffect } from "react";
+
+const NoMatch = () => {
+  const navigate = useNavigate();
+
+  // !!! 重新導引，這部分就需要 test-token 才能判斷要回 user 首頁還是登入頁，現在只能都先回登入頁
+  useEffect(() => {
+    navigate("/login");
+  }, [navigate]);
+
+  return <p>There's nothing here: 404!</p>;
+};
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<LoginPage />} />
-        <Route path="admin" element={<AdminLoginPage />}>
+        <Route path="admin">
+          <Route index element={<AdminLoginPage />} />
           <Route path="users" element={<AdminUsersPage />} />
           <Route path="tweets" element={<AdminTweetsPage />} />
-          {/* 若輸入 "admin/不符合的字串" 會被導引到 MainPage */}
-          <Route path="*" element={<AdminUsersPage />} />
         </Route>
+
         <Route path="regist" element={<RegisterPage />} />
         <Route path="login" element={<LoginPage />} />
+        <Route path="setting" element={<SettingPage />} />
         <Route path="main" element={<MainPage />} />
         <Route path="tweet/:id" element={<TweetPage />} />
-        <Route path="user/:id" element={<UserMainPage />}>
+
+        <Route path="user/:id">
+          <Route index element={<UserMainPage />} />
           <Route path="replys" element={<UserReplysPage />} />
           <Route path="likes" element={<UserLikesPage />} />
           <Route path="followers" element={<FollowersPage />} />
           <Route path="following" element={<FollowingPage />} />
-          {/* 若輸入 "user/:id/不符合的字串" 會被導引到 MainPage */}
-          <Route path="*" element={<MainPage />} />
         </Route>
-        <Route path="setting" element={<SettingPage />} />
+
         {/* * 代表除了以上設定好的路由，其他字串不符合的會被導引到 NoMatch */}
         <Route path="*" element={<NoMatch />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
-const NoMatch = () => {
-  return <p>There's nothing here: 404!</p>;
-};
 
 export default App;
