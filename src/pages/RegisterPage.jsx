f124599b5b8e2f7672e3bcfe4d19be27e28d9177
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-03-24 17:54:56 +0800

    feat: show message when register get error:
    根據以下五種狀況跳出錯誤訊息：
    沒填欄位、帳號已註冊、信箱已註冊、信箱格式錯誤、兩次輸入的密碼錯誤。
    並在伺服器出狀況時跳出通知，
    註冊成功則會跳出通知，並且跳轉到登入頁面。

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,6 +1,7 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { register } from "api/userAuth";
+import Swal from "sweetalert2";
 
 // components
 import AuthContainer from "components/containers/AuthContainer";
@@ -16,12 +17,15 @@ function RegisterPage() {
   const [password, setPassword] = useState("");
   const [checkPassword, setCheckPassword] = useState("");
 
+  const navigate = useNavigate();
+
   const handleAccountChange = (e) => {
     setAccount(e.target.value);
   };
 
-  const hideError = (el) => {
+  const clearError = (el) => {
     el.classList.remove("error");
+    el.parentElement.setAttribute("data-content", "");
   };
 
   const showError = (el, message) => {
@@ -31,10 +35,10 @@ function RegisterPage() {
 
   const handleNameChange = (e) => {
     //清掉前一次提醒
-    hideError(e.target);
+    clearError(e.target);
 
     //  超過 50 字的提醒
-    if (e.target.value.length > 3) {
+    if (e.target.value.length > 50) {
       showError(e.target, "您已輸入超過 50 字");
     }
 
@@ -55,9 +59,14 @@ function RegisterPage() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const inputEls = [...e.target.querySelectorAll("input")];
+    console.log(inputEls);
+
+    // 清除前一次的錯誤訊息
+    inputEls.forEach((inputEl) => clearError(inputEl));
 
     try {
-      const res = await register({
+      const { status, message } = await register({
         account,
         name,
         email,
@@ -65,7 +74,106 @@ function RegisterPage() {
         checkPassword,
       });
 
-      console.log(res);
+      // 欄位不得為空，去找到是否有至少一個欄位為空
+      if (inputEls.some((inputEl) => inputEl.value.length === 0)) {
+        // 找出欄位為空的
+        const emptyInputEls = inputEls.filter(
+          (inputEl) => inputEl.value.length === 0
+        );
+
+        // 針對欄位為空的跳 error
+        emptyInputEls.forEach((inputEl) => showError(inputEl, message));
+
+        return;
+      }
+
+      // !!! 目前以下 error message 的設計都比較 hard code，還可以再優化
+      // 帳號註冊過
+      if (message === "Account 重複註冊") {
+        const [accountInputEL] = inputEls.filter(
+          (inputEl) => inputEl.id === "regist_account"
+        );
+        showError(accountInputEL, message);
+        // showError(inputEls[1], message);
+        return;
+      }
+
+      // name 超過 50字
+      if (message === "名稱不能超過 50 個字") {
+        const [nameInputEL] = inputEls.filter(
+          (inputEl) => inputEl.id === "regist_name"
+        );
+        showError(nameInputEL, message);
+        // showError(inputEls[1], message);
+        return;
+      }
+
+      // 信箱格式錯誤
+      if (message === "Email 格式有誤") {
+        const [emailInputEL] = inputEls.filter(
+          (inputEl) => inputEl.id === "regist_email"
+        );
+        showError(emailInputEL, message);
+        // showError(inputEls[2], message);
+        return;
+      }
+
+      // 信箱註冊過
+      if (message === "Email 重複註冊") {
+        const [emailInputEL] = inputEls.filter(
+          (inputEl) => inputEl.id === "regist_email"
+        );
+        showError(emailInputEL, message);
+        // showError(inputEls[2], message);
+        return;
+      }
+
+      // 兩次輸入的密碼不同
+      if (message === "兩次輸入的密碼不相同") {
+        const [passwordInputEL] = inputEls.filter(
+          (inputEl) => inputEl.id === "regist_password"
+        );
+        const [checkPasswordInputEL] = inputEls.filter(
+          (inputEl) => inputEl.id === "regist_checkPassword"
+        );
+        showError(passwordInputEL, message);
+        showError(checkPasswordInputEL, message);
+
+        // showError(inputEls[3], message);
+        // showError(inputEls[4], message);
+        return;
+      }
+
+      // 伺服器有誤
+      if (message === "伺服器出現問題，請稍後再使用") {
+        Swal.fire({
+          position: "top",
+          icon: "warning",
+          title: message,
+          timer: 1500,
+          showConfirmButton: false,
+          customClass: {
+            icon: "swalIcon right",
+            title: "swalTitle",
+          },
+        });
+        return;
+      }
+
+      // 註冊成功，因為沒有 token ，跳轉登入頁面
+      Swal.fire({
+        position: "top",
+        icon: "success",
+        title: message,
+        timer: 1500,
+        showConfirmButton: false,
+        customClass: {
+          icon: "swalIcon right",
+          title: "swalTitle",
+        },
+      });
+
+      navigate("/login");
     } catch (error) {
       console.error(error);
     }
@@ -91,12 +199,11 @@ function RegisterPage() {
           placeholder="請設定使用者名稱，不可超過50字"
           value={name}
           onChange={handleNameChange}
-          // maxLength="50"
         />
         <Input
           id="regist_email"
           label="Email"
-          type="email"
+          type="text"
           placeholder="請輸入 Email"
           value={email}
           onChange={handleEmailChange}
