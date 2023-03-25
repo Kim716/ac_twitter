35cc70ea4a184db8787f637cb3fe0babd8cc4789
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-03-25 15:45:42 +0800

    feat: add SettingPage Header

--- src/pages/SettingPage.jsx
+++ src/pages/SettingPage.jsx
@@ -1,9 +1,27 @@
 import { NavBar } from "components/NavBar";
+import styled from "styled-components";
+
+// components
+import MainContainer from "components/containers/MainContainer";
+import Header from "components/Header";
+
+const StyledDiv = styled.div`
+  height: 100vh;
+  border-right: 1px solid var(--grey3);
+  overflow: auto;
+`;
 
 function SettingPage() {
   return (
     <div className="d-flex">
       <NavBar />
+      <MainContainer>
+        <StyledDiv className="col-8">
+          <Header>
+            <h1>帳戶設定</h1>
+          </Header>
+        </StyledDiv>
+      </MainContainer>
     </div>
   );
 }
