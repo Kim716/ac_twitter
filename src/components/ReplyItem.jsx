612cef6b62e2d341a5afbe0eb6e816c88cf9ddee
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-03-26 15:38:31 +0800

    feat: create ReplyItem component, combine TweetPage:
    使用後端格式跑假陣列 reply 資料，TweetPage 能正常顯示

---
+++ src/components/ReplyItem.jsx
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  background: var(--white);
+  padding: 16px 24px;
+  border-bottom: 1px solid var(--grey3);
+
+  img {
+    display: block;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    margin-right: 8px;
+  }
+
+  div {
+    .user_info,
+    .reply_to {
+      margin-bottom: 8px;
+      color: var(--secondary);
+      font-size: 14px;
+      line-height: 26px;
+      background: white;
+
+      .reply_name {
+        margin-right: 8px;
+        color: var(--grey9);
+        font-size: 16px;
+        font-weight: 700;
+      }
+
+      .reply_to_account {
+        color: var(--brand-color);
+      }
+    }
+
+    .comment {
+      color: var(--grey9);
+      line-height: 26px;
+      background: white;
+    }
+  }
+`;
+
+function ReplyItem({ avatar, name, account, createAt, replyTo, comment }) {
+  return (
+    <StyledDiv className="d-flex">
+      <img src={avatar} alt="avatar" />
+
+      <div>
+        <p className="user_info">
+          <span className="reply_name">{name}</span>@{account}・{createAt}
+        </p>
+        <p className="reply_to">
+          回覆 <span className="reply_to_account">@{replyTo}</span>
+        </p>
+        <p className="comment">{comment}</p>
+      </div>
+    </StyledDiv>
+  );
+}
+
+export default ReplyItem;
--- src/pages/TweetPage.jsx
+++ src/pages/TweetPage.jsx
@@ -2,8 +2,34 @@ import MainContainer from "components/containers/MainContainer";
 import ViewContainer from "components/containers/ViewContainer";
 import Header from "components/Header";
 import { NavBar } from "components/NavBar";
+import ReplyItem from "components/ReplyItem";
 import SideBar from "components/SideBar";
 import TweetCard from "components/TweetCard";
+
+const dummyReplys = [
+  {
+    id: 590,
+    comment: "Sequi reiciendis quaerat voluptas enim.",
+    UserId: 2,
+    TweetId: 45,
+    createdAt: "下午 06:49 2023年03月12日",
+    updatedAt: "2023-03-23T23:55:46.000Z",
+    User: {
+      id: 2,
+      name: "user1",
+      account: "user1",
+      avatar: "https://loremflickr.com/320/240/man,woman/?lock=46",
+    },
+    Tweet: {
+      id: 45,
+      UserId: 6,
+      User: {
+        id: 6,
+        account: "user5",
+      },
+    },
+  },
+];
 
 function TweetPage() {
   return (
@@ -15,6 +41,17 @@ function TweetPage() {
             <h1>推文</h1>
           </Header>
           <TweetCard />
+          {/* 以下會跑 map */}
+          {dummyReplys.map((reply) => (
+            <ReplyItem
+              avatar={reply.User.avatar}
+              name={reply.User.name}
+              account={reply.User.account}
+              createAt={reply.createdAt}
+              replyTo={reply.Tweet.User.account}
+              comment={reply.comment}
+            />
+          ))}
         </ViewContainer>
         <SideBar />
       </MainContainer>
