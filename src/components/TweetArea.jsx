e66d7b4652f3f8cccaf0e8a1408916fcf650ad83
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-03-27 23:05:46 +0800

    fix: eslint

--- src/components/TweetArea.jsx
+++ src/components/TweetArea.jsx
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import Avatar from "assets/images/avatar.png";
 import ActButton from "components/ActButton";
 
