f5d5a63980c5bb221f3d08267dac0cfff8f95934
Kim <yichen.hsieh_c7_alumni@teach4taiwan.org>
2023-03-30 16:23:00 +0800

    feat: if admin already login go to main page

--- src/pages/AdminLoginPage.jsx
+++ src/pages/AdminLoginPage.jsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { adminLogin } from "api/adminAuth";
 import Swal from "sweetalert2";
 
@@ -86,6 +86,28 @@ function AdminLoginPage() {
       console.error(error);
     }
   };
+
+  // 簡易前端驗證，如果管理者已經是登入狀態，localStorage 會有這兩個東西，就直接導入前台首頁
+  useEffect(() => {
+    const adminToken = localStorage.getItem("adminToken");
+
+    if (adminToken) {
+      // 跳通知
+      Swal.fire({
+        position: "top",
+        icon: "success",
+        title: "您已成功登入",
+        timer: 1500,
+        showConfirmButton: false,
+        customClass: {
+          icon: "swalIcon right",
+          title: "swalTitle",
+        },
+      });
+
+      navigate("/admin/tweets");
+    }
+  }, [navigate]);
 
   return (
     <AuthContainer>
